fb560491616e141961a158587ca7c56ffb94c7a9
William <williamw15@protonmail.com>
2023-02-09 23:17:04 -0500

    add routing to application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 /** @format */
 
+import { Routes, Route } from 'react-router-dom';
+
+import LandingPage from './components/landing-page';
+import SignIn from './components/sign-in';
+import SignUp from './components/sign-up';
+import LoggedIn from './components/logged-in';
+import PageNotFound from './components/page-not-found';
+
 function App() {
   return (
-    <div className='App'>
-      <h1>App works</h1>
-    </div>
+    <Routes>
+      <Route
+        path='/'
+        element={<LandingPage />}
+      />
+      <Route
+        path='/sign-in'
+        element={<SignIn />}
+      />
+      <Route
+        path='/sign-up'
+        element={<SignUp />}
+      />
+      <Route
+        path='/logged-in'
+        element={<LoggedIn />}
+      />
+      <Route
+        path='*'
+        element={<PageNotFound />}
+      />
+    </Routes>
   );
 }
 
