446daa637eec953636e640243817937c3bf54552
William <williamw15@protonmail.com>
2023-02-09 23:11:53 -0500

    navigation header finished, with styling completed

---
+++ src/components/navigation/index.jsx
@@ -0,0 +1,31 @@
+/** @format */
+
+import './style.css';
+
+import { NavLink } from 'react-router-dom';
+
+export default function Navigation() {
+  return (
+    <div className='container'>
+      <span className='webpage-name'>Diabetic Dynamics</span>
+      <span className='button-container'>
+        <button className='button-style'>
+          <NavLink
+            className='link-text'
+            to='sign-in'
+          >
+            Sign-In
+          </NavLink>
+        </button>
+        <button className='button-style'>
+          <NavLink
+            className='link-text'
+            to='sign-up'
+          >
+            Sign Up
+          </NavLink>
+        </button>
+      </span>
+    </div>
+  );
+}
