74d8e4bb5603612d6958abcbab10b475c85d3e92
William <williamw15@protonmail.com>
2023-04-09 17:22:06 -0400

    navigation buttons updated with navigate hook, replacing Link

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,9 +1,16 @@
-import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import SignOut from '../sign-out';
 import './styles.scss';
 
+/**
+ * Displays the navigation bar, with navigation links.
+ * @returns
+ * - If the user is not signed in, displays the sign-in, sign-up buttons.
+ * - If the user is signed, displays the sign-out button.
+ */
 export default function Navigation() {
+  const navigate = useNavigate();
   const loggedInStatus = useSelector((state) => state.loggedInStatus.loggedIn);
 
   return (
@@ -14,11 +21,17 @@ export default function Navigation() {
           <SignOut />
         ) : (
           <>
-            <button className='button-sign-in'>
-              <NavLink to='sign-in'>Sign-In</NavLink>
+            <button
+              className='button-sign-in'
+              onClick={() => navigate('sign-in')}
+            >
+              Sign-In
             </button>
-            <button className='button-sign-up'>
-              <NavLink to='sign-up'>Sign Up</NavLink>
+            <button
+              className='button-sign-up'
+              onClick={() => navigate('sign-up')}
+            >
+              Sign Up
             </button>
           </>
         )}
