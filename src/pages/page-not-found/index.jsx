a18dd9ef787e9671ef5d89967e04b62f7d656211
William <williamw15@protonmail.com>
2023-04-09 17:18:07 -0400

    page not found styled, link added to navigate user to home page

--- src/pages/page-not-found/index.jsx
+++ src/pages/page-not-found/index.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import Card from './../../components/Card';
+import { useNavigate } from 'react-router-dom';
+import './styles.scss';
 
-class PageNotFound extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Page Not Found works</h1>
-      </div>
-    );
-  }
-}
+const PageNotFound = () => {
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate('/');
+  };
+
+  return (
+    <>
+      <Card>
+        <div className='page-not-found-container'>
+          <p>
+            Oops! We're sorry, but the page you're looking for cannot be found.
+            It may have been moved, deleted, or never existed in the first
+            place. Please check the URL or use the navigation menu to find what
+            you're looking for. If you continue to have trouble, please contact
+            our support team for assistance.
+          </p>
+          <button onClick={navigateHandler}>Home</button>
+        </div>
+      </Card>
+    </>
+  );
+};
 
 export default PageNotFound;
