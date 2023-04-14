bd155b99e41694736c8dcce771e58482cb6a4c9e
William <williamw15@protonmail.com>
2023-04-14 14:55:31 -0400

    add user automatic log in and redirect to pets-list page when user successfully creates an account

--- src/components/sign-up/index.jsx
+++ src/components/sign-up/index.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { Link } from 'react-router-dom';
 import { authentication } from './../../api/FirebaseConfig';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 import Card from '../card';
 import './styles.scss';
 
@@ -16,6 +18,7 @@ export default function SignUp() {
   const [password, setPassword] = useState('Temppassword1!');
   const [reEnteredPassword, setReEnteredPassword] = useState('');
   const [errorMessages, setErrorMessages] = useState([]);
+  const userID = useSelector((state) => state.userID.id);
 
   /**
    * Function that is invoked when the user clicks the Create Account button.
@@ -77,90 +80,96 @@ export default function SignUp() {
   };
 
   return (
-    <Card>
-      <form className='sign-up-container'>
-        <h1 className='sign-up-text'>Sign up</h1>
-        <div className='error-messages-container'>
-          {/* Error Messages when the entered information doesn't pass the checks. */}
-          {errorMessages.length > 0 && (
-            <div>
-              <h3>Invalid Data:</h3>
-              <ul>
-                {errorMessages.map((error, index) => {
-                  return <li key={index}>{error}</li>;
-                })}
-              </ul>
+    <>
+      {userID ? (
+        <Navigate to='/pets-list' />
+      ) : (
+        <Card>
+          <form className='sign-up-container'>
+            <h1 className='sign-up-text'>Sign up</h1>
+            <div className='error-messages-container'>
+              {/* Error Messages when the entered information doesn't pass the checks. */}
+              {errorMessages.length > 0 && (
+                <div>
+                  <h3>Invalid Data:</h3>
+                  <ul>
+                    {errorMessages.map((error, index) => {
+                      return <li key={index}>{error}</li>;
+                    })}
+                  </ul>
+                </div>
+              )}
             </div>
-          )}
-        </div>
-        <div className='input-container'>
-          <div>
-            {/* Form for input of email address. */}
-            <label className='label-text'>
-              Email Address:
+            <div className='input-container'>
               <div>
-                <input
-                  className='input-field'
-                  type='email'
-                  value={emailAddress}
-                  onChange={(e) => {
-                    setEmailAddress(e.target.value);
-                  }}
-                />
+                {/* Form for input of email address. */}
+                <label className='label-text'>
+                  Email Address:
+                  <div>
+                    <input
+                      className='input-field'
+                      type='email'
+                      value={emailAddress}
+                      onChange={(e) => {
+                        setEmailAddress(e.target.value);
+                      }}
+                    />
+                  </div>
+                </label>
               </div>
-            </label>
-          </div>
-          <div>
-            {/* Form for input of password. */}
-            <label className='label-text'>
-              Password:
               <div>
-                <input
-                  className='input-field'
-                  type='password'
-                  value={password}
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
-                />
+                {/* Form for input of password. */}
+                <label className='label-text'>
+                  Password:
+                  <div>
+                    <input
+                      className='input-field'
+                      type='password'
+                      value={password}
+                      onChange={(e) => {
+                        setPassword(e.target.value);
+                      }}
+                    />
+                  </div>
+                </label>
               </div>
-            </label>
-          </div>
-          <div>
-            {/* Form for input of re-entered password. */}
-            <label className='label-text'>
-              Re-enter Password:
               <div>
-                <input
-                  className='input-field'
-                  type='password'
-                  value={reEnteredPassword}
-                  onChange={(e) => {
-                    setReEnteredPassword(e.target.value);
-                  }}
-                />
+                {/* Form for input of re-entered password. */}
+                <label className='label-text'>
+                  Re-enter Password:
+                  <div>
+                    <input
+                      className='input-field'
+                      type='password'
+                      value={reEnteredPassword}
+                      onChange={(e) => {
+                        setReEnteredPassword(e.target.value);
+                      }}
+                    />
+                  </div>
+                </label>
               </div>
-            </label>
-          </div>
-          <div className='button-container'>
-            {/* Buttons which handles the creation of the account for the user. */}
-            <button
-              className='create-account-button'
-              onClick={(e) => createAccount(e)}
-            >
-              Create Account
-            </button>
-            {/* Link for the user to navigate to the sign in page.  */}
-            <div className='sign-in-link'>
-              Already have an account? <Link to='/sign-in'>Sign In</Link>
+              <div className='button-container'>
+                {/* Buttons which handles the creation of the account for the user. */}
+                <button
+                  className='create-account-button'
+                  onClick={(e) => createAccount(e)}
+                >
+                  Create Account
+                </button>
+                {/* Link for the user to navigate to the sign in page.  */}
+                <div className='sign-in-link'>
+                  Already have an account? <Link to='/sign-in'>Sign In</Link>
+                </div>
+                {/* Link for the user to navigate to the home page. */}
+                <div className='home-link'>
+                  Click <Link to='/'>here</Link> to go back home
+                </div>
+              </div>
             </div>
-            {/* Link for the user to navigate to the home page. */}
-            <div className='home-link'>
-              Click <Link to='/'>here</Link> to go back home
-            </div>
-          </div>
-        </div>
-      </form>
-    </Card>
+          </form>
+        </Card>
+      )}
+    </>
   );
 }
